8e2e0b4efd26ff9875b85ade2420f35ed9be49e3
Oricul <Oricul@users.noreply.github.com>
2017-10-04 22:52:19 -0500

    Add recheck command.

--- modules/firstIter.py
+++ modules/firstIter.py
@@ -43,46 +43,44 @@ class test:
                         allFound = (await resp.json())['data']
                         found = allFound['organizations']
                         session.close()
-                        orgFound = 0
-                        for i in found:
-                            if (i['sid'] != ""):
-                                if (i['sid'].upper() == orgSID.upper()):
-                                    orgFound = 1
+                    orgFound = 0
+                    for i in found:
+                        if (i['sid'] != ""):
+                            if (i['sid'].upper() == orgSID.upper()):
+                                orgFound = 1
+                                break
+                    if orgFound:
+                        if (found[0]['sid'].upper() == orgSID.upper()):
+                            await self.bot.add_reaction(message,"{}".format(approveEmoji))
+                            embed = discord.Embed(colour=discord.Colour(0x00FF00),
+                                                  description="You've been detected as a member. Welcome aboard.")
+                            embed.set_author(name=allFound['handle'].title(),icon_url=allFound['avatar'])
+                            embed.set_thumbnail(url=allFound['avatar'])
+                            for i in message.server.roles:
+                                if (approveRole.upper() == i.name.upper()):
+                                    await self.bot.add_roles(message.author,i)
                                     break
-                        if orgFound:
-                            if (found[0]['sid'].upper() == orgSID.upper()):
-                                await self.bot.add_reaction(message,"{}".format(approveEmoji))
-                                embed = discord.Embed(colour=discord.Colour(0x00FF00),
-                                                      description="You've been detected as a member. Welcome aboard.")
-                                embed.set_author(name=allFound['handle'].title(),icon_url=allFound['avatar'])
-                                embed.set_thumbnail(url=allFound['avatar'])
-                                for i in message.server.roles:
-                                    if (approveRole.upper() == i.name.upper()):
-                                        await self.bot.add_roles(message.author,i)
-                                        break
-                                await self.bot.change_nickname(message.author,foundHandle)
-                                await self.bot.send_message(message.channel,embed=embed)
-                            else:
-                                await self.bot.add_reaction(message,"{}".format(affiliateEmoji))
-                                embed = discord.Embed(title=message.author.name, colour=discord.Colour(0x00FF00),
-                                                      url=correctURL,
-                                                      description="You've been detected as a affiliate. Welcome.")
-                                embed.set_author(name=allFound['handle'].title(), icon_url=allFound['avatar'])
-                                embed.set_thumbnail(url=allFound['avatar'])
-                                for i in message.server.roles:
-                                    if (affiliateRole.upper() == i.name.upper()):
-                                        await self.bot.add_roles(message.author,i)
-                                        break
-                                await self.bot.change_nickname(message.author, foundHandle)
-                                await self.bot.send_message(message.channel,embed=embed)
+                            await self.bot.change_nickname(message.author,foundHandle)
+                            await self.bot.send_message(message.channel,embed=embed)
                         else:
-                            await self.bot.add_reaction(message,"{}".format(pendingEmoji))
-                            embed = discord.Embed(title=message.author.name, colour=discord.Colour(0xFFFF00),
-                                                  url=correctURL,
-                                                  description="Pending manual verification, please standby.")
+                            await self.bot.add_reaction(message,"{}".format(affiliateEmoji))
+                            embed = discord.Embed(colour=discord.Colour(0x00FF00),
+                                                  description="You've been detected as an affiliate. Welcome.")
                             embed.set_author(name=allFound['handle'].title(), icon_url=allFound['avatar'])
                             embed.set_thumbnail(url=allFound['avatar'])
+                            for i in message.server.roles:
+                                if (affiliateRole.upper() == i.name.upper()):
+                                    await self.bot.add_roles(message.author,i)
+                                    break
+                            await self.bot.change_nickname(message.author, foundHandle)
                             await self.bot.send_message(message.channel,embed=embed)
+                    else:
+                        await self.bot.add_reaction(message,"{}".format(pendingEmoji))
+                        embed = discord.Embed(colour=discord.Colour(0xFFFF00),
+                                              description="Pending manual verification, please standby.")
+                        embed.set_author(name=allFound['handle'].title(), icon_url=allFound['avatar'])
+                        embed.set_thumbnail(url=allFound['avatar'])
+                        await self.bot.send_message(message.channel,embed=embed)
         pass
 
     @commands.command(pass_context=True,hidden=True)
@@ -112,6 +110,65 @@ class test:
             else:
                 await self.bot.send_message(ctx.message.author,"You are not authorized to use this command.")
         return
+
+    @commands.command(pass_context=True,hidden=True)
+    async def recheck(self,ctx,userName,handle):
+        try:
+            await self.bot.delete_message(ctx.message)
+        except:
+            pass
+        if (ctx.message.author.bot == False):
+            if (ctx.message.author.server_permissions.administrator == True):
+                session = aiohttp.ClientSession()
+                getURL = "http://sc-api.com/?data_source=RSI&api_source=live&system=accounts&action=full_profile&target_id={}".format(
+                    handle)
+                headers = {'content-type': 'application/json'}
+                async with session.post(getURL, headers=headers) as resp:
+                    allFound = (await resp.json())['data']
+                    found = allFound['organizations']
+                    session.close()
+                orgFound = 0
+                orgList = []
+                for i in found:
+                    if (i['sid'] != ""):
+                        orgList.append(i['sid'])
+                        if (i['sid'].upper() == orgSID.upper()):
+                            orgFound = 1
+                            break
+                if orgFound:
+                    if (found[0]['sid'].upper() == orgSID.upper()):
+                        await self.bot.add_reaction(ctx.message, "{}".format(approveEmoji))
+                        embed = discord.Embed(colour=discord.Colour(0x00FF00),
+                                              description="{} has been detected as a member.".format(handle))
+                        embed.set_author(name=allFound['handle'].title(), icon_url=allFound['avatar'])
+                        embed.set_thumbnail(url=allFound['avatar'])
+                        for i in ctx.message.server.roles:
+                            if (approveRole.upper() == i.name.upper()):
+                                await self.bot.add_roles(userName, i)
+                                break
+                        await self.bot.change_nickname(userName, handle)
+                        await self.bot.send_message(ctx.message.channel, embed=embed)
+                    else:
+                        await self.bot.add_reaction(ctx.message, "{}".format(affiliateEmoji))
+                        embed = discord.Embed(colour=discord.Colour(0x00FF00),
+                                              description="{} has been detected as an affiliate.".format(handle))
+                        embed.set_author(name=allFound['handle'].title(), icon_url=allFound['avatar'])
+                        embed.set_thumbnail(url=allFound['avatar'])
+                        for i in ctx.message.server.roles:
+                            if (affiliateRole.upper() == i.name.upper()):
+                                await self.bot.add_roles(userName, i)
+                                break
+                        await self.bot.change_nickname(userName, handle)
+                        await self.bot.send_message(ctx.message.channel, embed=embed)
+                else:
+                    await self.bot.add_reaction(ctx.message, "{}".format(pendingEmoji))
+                    embed = discord.Embed(colour=discord.Colour(0xFFFF00),
+                                          description="Still not found. Please verify manually.")
+                    embed.set_author(name=allFound['handle'].title(), icon_url=allFound['avatar'])
+                    embed.set_thumbnail(url=allFound['avatar'])
+                    await self.bot.send_message(ctx.message.channel, embed=embed)
+        return
+
 
 def setup(bot):
     bot.add_cog(test(bot))
